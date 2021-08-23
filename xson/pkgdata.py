07fbc67025f4e6d10865a4325629eef90caeca48
Akos Kiss <akiss@inf.u-szeged.hu>
2021-08-24 00:05:56 +0200

    Use importlib.metadata to retrieve package version
    (Use importlib_metadata backport for <py38.) This allows to get rid
    of pkg_resources/setuptools install dependency.

--- xson/pkgdata.py
+++ xson/pkgdata.py
@@ -1,14 +1,17 @@
-# Copyright (c) 2019 Renata Hodovan, Akos Kiss.
+# Copyright (c) 2019-2021 Renata Hodovan, Akos Kiss.
 #
 # Licensed under the BSD 3-Clause License
 # <LICENSE.rst or https://opensource.org/licenses/BSD-3-Clause>.
 # This file may not be copied, modified, or distributed except
 # according to those terms.
 
-import pkg_resources
+try:
+    from importlib import metadata
+except ImportError:
+    import importlib_metadata as metadata
 
 
-__version__ = pkg_resources.get_distribution(__package__).version
+__version__ = metadata.version(__package__)
 
 JSONX_PREFIX = 'json'
 JSONX_NS_URI = 'http://www.ibm.com/xmlns/prod/2009/jsonx'
